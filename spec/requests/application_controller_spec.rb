ec8e77907b8c13d3fee2abd3ee4d095e1d3a8ba7
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-11-02 19:09:25 -0700

    Write test for trivia endpoint Co-authored-by: Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com> Co-authored-by: Daniel Halverson <63027888+dhalverson@users.noreply.github.com> Co-authored-by: Tyler Fields <64871701+fieldstyler@users.noreply.github.com>

--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -1,15 +1,33 @@
 require "./spec/spec_helper"
-require "./app/controllers/application_controller"
-require 'sinatra'
+
 def app
   ApplicationController
 end
 
-describe "ApplicationController" do
+RSpec.describe "ApplicationController" do
   it "responds with a welcome message" do
-    # get "/"
-    get "/api/v1/pom"
-    expect(last_response.status).to eq(200)
+    get "/api/v1/single_trivia_question"
+
     body = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(last_response.status).to eq(200)
+    expect(body).to have_key(:response_code)
+    expect(body[:response_code]).to eq(0)
+    expect(body).to have_key(:results)
+    expect(body[:results]).to be_an(Array)
+    expect(body[:results].first).to be_a(Hash)
+    expect(body[:results].first).to have_key(:category)
+    expect(body[:results].first[:category]).to be_a(String)
+    expect(body[:results].first).to have_key(:type)
+    expect(body[:results].first[:type]).to be_a(String)
+    expect(body[:results].first).to have_key(:difficulty)
+    expect(body[:results].first[:difficulty]).to be_a(String)
+    expect(body[:results].first).to have_key(:question)
+    expect(body[:results].first[:question]).to be_a(String)
+    expect(body[:results].first).to have_key(:correct_answer)
+    expect(body[:results].first[:correct_answer]).to be_a(String)
+    expect(body[:results].first).to have_key(:incorrect_answers)
+    expect(body[:results].first[:incorrect_answers]).to be_an(Array)
+    
   end
 end
